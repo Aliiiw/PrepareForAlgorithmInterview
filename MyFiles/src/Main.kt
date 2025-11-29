d42d10fd113c52b16ca79ea9327ed55a45a4d934
Ali Rahimi <ali.rahimi@gandom.co>
2025-11-29 14:30:25 +0100

    add file

--- MyFiles/src/Main.kt
+++ MyFiles/src/Main.kt
@@ -1,18 +1,40 @@
 fun main() {
-    println(fizzBuzz(15))
+
 }
 
-fun fizzBuzz(n: Int): List<String> {
-    val items = mutableListOf<String>()
-    for (i in 1..n) {
-        if (i % 15 == 0)
-            items.add("FizzBuzz")
-        else if (i % 3 == 0)
-            items.add("Fizz")
-        else if (i % 5 == 0)
-            items.add("Buzz")
-        else items.add(i.toString())
-    }
-    return items
-}
+
+//fun reverseString(s: CharArray): Unit {
+//    var left = 0
+//    var right = s.size - 1
+//
+//    while (left < right) {
+//        val temp = s[left]
+//        s[left] = s[right]
+//        s[right] = temp
+//        left++
+//        right--
+//    }
+//}
+//
+//fun reverseString(s: CharArray): Unit {
+//    val result = mutableListOf<String>()
+//    for(i in s.size - 1 downTo 0) {
+//        result.add(s[i].toString())
+//    }
+//    println(result)
+//}
+//
+//fun fizzBuzz(n: Int): List<String> {
+//    val items = mutableListOf<String>()
+//    for (i in 1..n) {
+//        if (i % 15 == 0)
+//            items.add("FizzBuzz")
+//        else if (i % 3 == 0)
+//            items.add("Fizz")
+//        else if (i % 5 == 0)
+//            items.add("Buzz")
+//        else items.add(i.toString())
+//    }
+//    return items
+//}
 
