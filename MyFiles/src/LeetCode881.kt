b15b355da72401125bed0eed88e3d6b5d7039736
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-01 12:03:57 +0100

    add files

---
+++ MyFiles/src/LeetCode881.kt
@@ -0,0 +1,19 @@
+fun numRescueBoats(people: IntArray, limit: Int): Int {
+    var boats = 0
+    var start = 0
+    var end = people.size - 1
+
+    people.sort()
+
+    while (end >= start) {
+        if (people[end] + people[start] <= limit) {
+            start++
+            end--
+            boats++
+        } else {
+            boats++
+            end--
+        }
+    }
+    return boats
+}
