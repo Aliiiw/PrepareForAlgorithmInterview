2fb9a2f226511adcd3b78bdf4b597ce41c175724
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-20 19:23:55 +0100

    add files

---
+++ MyFiles/src/leet_code/LeetCode80.kt
@@ -0,0 +1,20 @@
+package leet_code
+
+fun main() {
+    val nums = intArrayOf(1, 1, 1, 2, 2, 3)
+    print(removeDuplicatesII(nums))
+}
+
+
+fun removeDuplicatesII(nums: IntArray): Int {
+    var write = 0
+
+    for (number in nums) {
+        if (write < 2 || number != nums[write - 2]) {
+            nums[write] = number
+            write++
+        }
+    }
+    return write
+}
+
