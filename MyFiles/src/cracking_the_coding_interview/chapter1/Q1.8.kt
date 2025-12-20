5bf23a2a54861b675e8eeba7b173b8088701e14c
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-20 16:18:29 +0100

    add 1.8

---
+++ MyFiles/src/cracking_the_coding_interview/chapter1/Q1.8.kt
@@ -0,0 +1,12 @@
+package cracking_the_coding_interview.chapter1
+
+/*
+Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0,
+its entire row and column are set to 0.
+
+ */
+
+fun main() {
+
+}
+
