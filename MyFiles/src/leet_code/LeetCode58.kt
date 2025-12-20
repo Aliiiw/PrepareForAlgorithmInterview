818cb225c75446f887e74709ddcc2358ca434da5
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-20 18:39:00 +0100

    add files

---
+++ MyFiles/src/leet_code/LeetCode58.kt
@@ -0,0 +1,31 @@
+package leet_code
+
+
+fun main() {
+    val test = "   fly me   to   the moon  "
+
+    print(lengthOfLastWord2(test))
+
+}
+
+// v1
+fun lengthOfLastWord(s: String): Int {
+    return s.trim().split(" ").last().length
+}
+
+// v2
+fun lengthOfLastWord2(s: String): Int {
+    var i = s.length - 1
+    while (i >= 0 && s[i] == ' ') i--
+    var len = 0
+    while (i >= 0 && s[i] != ' ') {
+        len++
+        i--
+    }
+    return len
+}
+
+
+
+
+
