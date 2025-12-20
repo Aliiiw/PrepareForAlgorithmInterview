7f798ba049a348f9f175e02484fd4f7b7efa91b3
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-20 17:48:06 +0100

    add files

--- MyFiles/src/leet_code/LeetCode169.kt
+++ MyFiles/src/leet_code/LeetCode169.kt
@@ -1,5 +1,12 @@
 package leet_code
 
+
+fun main() {
+    val test = intArrayOf(2, 2, 1, 1, 1, 2, 2)
+    print(majorityElement2(test))
+}
+
+// v1
 fun majorityElement(nums: IntArray): Int {
     val map = HashMap<Int, Int>()
     val n = nums.size
@@ -20,4 +27,23 @@ fun majorityElement(nums: IntArray): Int {
         }
     }
     return majority
+}
+
+// v2
+fun majorityElement2(nums: IntArray): Int {
+
+    if (nums.isEmpty()) return 0
+
+    var candidate = 0
+    var counter = 0
+
+    nums.forEach { number ->
+        if (counter == 0) {
+            candidate = number
+        }
+        if (number == candidate) counter++
+        else counter--
+    }
+
+    return candidate
 }
