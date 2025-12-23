3d852ecd359026f729b534088c5186b093979d0e
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-23 11:29:32 +0100

    create files

---
+++ MyFiles/src/leet_code/LeetCode209.kt
@@ -0,0 +1,29 @@
+package leet_code
+
+
+fun main() {
+    print(minSubArrayLen(7, intArrayOf(2, 3, 1, 2, 4, 3)))
+}
+
+
+fun minSubArrayLen(target: Int, nums: IntArray): Int {
+    var left = 0
+    var sum = 0L
+    var best = Int.MAX_VALUE
+
+    for (right in nums.indices) {
+        sum += nums[right].toLong()
+
+        while (sum >= target.toLong()) {
+            val len = right - left + 1
+            if (len < best) best = len
+            sum -= nums[left].toLong()
+            left++
+        }
+    }
+
+    return if (best == Int.MAX_VALUE) 0 else best
+}
+
+
+
