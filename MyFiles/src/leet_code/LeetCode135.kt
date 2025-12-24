a4c9da6e73f2a70d9a3dedc93b8c22cc6fffb9f2
Ali Rahimi <ali.rahimi@gandom.co>
2025-12-24 10:33:39 +0100

    create files

---
+++ MyFiles/src/leet_code/LeetCode135.kt
@@ -0,0 +1,28 @@
+package leet_code
+
+
+fun main() {
+    print(candy(intArrayOf(1, 0, 2)))
+}
+
+fun candy(ratings: IntArray): Int {
+    val n = ratings.size
+    val candies = IntArray(n) { 1 }
+
+    for (i in 1 until n) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1
+        }
+    }
+
+    for (i in n - 2 downTo 0) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = maxOf(candies[i], candies[i + 1] + 1)
+        }
+    }
+
+    var sum = 0
+    for (c in candies) sum += c
+    return sum
+}
+
